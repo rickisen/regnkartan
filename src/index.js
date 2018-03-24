5ee8299bf3ac8a9abd1cfdbeac813d46cf39bb5d
rickisen <rickard.petersen@gmail.com>
2018-03-24 14:42:56 +0100

    Add react-router boilerplate

--- src/index.js
+++ src/index.js
@@ -1,15 +1,55 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+import {
+  StyleSheet,
+  Text,
+  View,
+  AppRegistry,
+} from 'react-native'
+import {
+  NativeRouter,
+  Route,
+  Link,
+} from 'react-router-native'
 
 import createStore from './redux'
 import Counters from './containers/Counters'
 
 const store = createStore()
 
-const Main = () => (
+const About = () => (
+  <View>
+    <Text>Om appen</Text>
+  </View>
+)
+
+const Nav = ({ match }) => (
+  <View>
+    <Link
+      to="/"
+      underlayColor='#f0f4f7'
+    >
+      <Text>Karta</Text>
+    </Link>
+    <Link
+      to="/about"
+      underlayColor='#f0f4f7'
+    >
+      <Text>About</Text>
+    </Link>
+  </View>
+)
+
+export default Main = () => (
   <Provider store={store}>
-    <Counters />
+    <NativeRouter>
+      <View>
+        <Nav/>
+        <Route exact path="/" component={Counters}/>
+        <Route path="/about" component={About}/>
+      </View>
+    </NativeRouter>
   </Provider>
 )
 
-export default Main
+// AppRegistry.registerComponent('regnkartan', () => Main);
