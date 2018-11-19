6943fd897b1435997477a98ae9e966d08bde26d0
rickisen <rickard.petersen@gmail.com>
2018-11-19 14:00:56 +0100

    update to sdk 31, and remove router

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,17 +1,13 @@
-import React from 'react'
-import { PropTypes } from 'prop-types'
-import {
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native'
+import React from 'react';
+import {PropTypes} from 'prop-types';
+import {StyleSheet, Text, View} from 'react-native';
 
-export default class Nav extends React.Component {
+export default class About extends React.Component {
   render() {
     return (
       <View>
         <Text>Om appen</Text>
       </View>
-    )
+    );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,27 +1,21 @@
-import React from 'react'
-import { Provider } from 'react-redux'
-import { View } from 'react-native'
-import { NativeRouter, Route } from 'react-router-native'
-import createAppStore from './redux' // pun intended
+import React from 'react';
+import {Provider} from 'react-redux';
+import {View} from 'react-native';
+import createAppStore from './redux'; // pun intended
 
-import Nav from './components/Nav'
-import Radar from './containers/Radar'
-import About from './containers/About'
+import Radar from './containers/Radar';
+import About from './containers/About';
 
-const store = createAppStore()
+const store = createAppStore();
 
 export default class Main extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <NativeRouter>
-          <View style={{ height: '100%' }}>
-            <Route exact path="/" component={Radar}/>
-            <Route path="/about" component={About}/>
-            <Nav/>
-          </View>
-        </NativeRouter>
+        <View style={{height: '100%'}}>
+          <Radar />
+        </View>
       </Provider>
-    )
+    );
   }
 }
