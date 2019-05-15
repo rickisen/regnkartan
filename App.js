5754ee646b52f97b771bdfbeed80a5217b479fe9
rickisen <rickard.petersen@gmail.com>
2019-05-15 16:13:56 +0200

    Clear the console on startup

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import Main from "./src";
 export default class App extends Component {
   // eslint-disable-line react/prefer-stateless-function
   render() {
+    console.clear();
     return <Main />;
   }
 }
