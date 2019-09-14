3289839bdf9831f9d04a37cdcc1725a624454b9a
rickisen <rickard.petersen@gmail.com>
2019-09-14 14:56:16 +0200

    Fix crashing error on Android

--- src/containers/Radar/RadarMap/RadarOverlay.js
+++ src/containers/Radar/RadarMap/RadarOverlay.js
@@ -11,14 +11,7 @@ const BOUNDS = [[70.3891859, 5.090008], [53.1219345, 30.1889371]];
 function RadarOverlay() {
   const uri = useSelector(({ radarSelection: { uri } }) => uri);
   const time = new Date(timeFromFilePath(uri));
-  return (
-    <>
-      <Text style={{ position: "absolute", top: 200, left: 100 }}>
-        {`${time.getHours()}:${time.getMinutes()}`}
-      </Text>
-      <MapView.Overlay image={{ uri }} bounds={BOUNDS} />
-    </>
-  );
+  return <MapView.Overlay image={{ uri }} bounds={BOUNDS} />;
 }
 
 export default RadarOverlay;
