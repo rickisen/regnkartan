818188c6b77c1312eb3d22ecce46bf3e2fd22a1b
rickisen <rickard.petersen@gmail.com>
2019-11-13 16:39:02 +0100

    Find closest png alternative when scrolling too fast and there are too few pngs

--- src/redux/modules/rainRadar/actionCreators.js
+++ src/redux/modules/rainRadar/actionCreators.js
@@ -8,6 +8,7 @@ export function registerHour(hourStamp) {
 export function registerTime(chunks, stamp) {
   let uri = null;
   const dateCode = generateDateCode(stamp, true, true);
+  const hourDateCode = generateDateCode(stamp, true);
   for (var hour in chunks) {
     const chunk = chunks[hour];
     const chunkBegin = parseInt(hour);
@@ -19,6 +20,15 @@ export function registerTime(chunks, stamp) {
       stamp < chunkEnd
     ) {
       uri = chunk.unpackedFiles.find(p => p.includes(dateCode));
+      if (!uri) {
+        const alternatives = chunk.unpackedFiles.filter(p =>
+          p.includes(hourDateCode)
+        );
+        // Length condtion is simple fix for rewind stutter when random pngs drop
+        if (alternatives.length === 1) {
+          uri = alternatives[0];
+        }
+      }
     }
   }
   return { type: SELECT_FILE, uri, stamp, dateCode };
